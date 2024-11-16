06e9800a7a19a1171af53c2b621e4eca48fb05d1
Jafar Loka <gaafer.hajji1995@gmail.com>
2024-11-16 18:30:53 +0200

    Create New Model Class And Migration The Data

--- my_api/first_api/models.py
+++ my_api/first_api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class JLCarSpecs(models.Model):
+    car_brand           = models.CharField(max_length=50);
+    car_model           = models.CharField(max_length=100);
+    production_year     = models.CharField(max_length=10);
+    car_body            = models.CharField(max_length=100);
+    engine_type         = models.CharField(max_length=50);
