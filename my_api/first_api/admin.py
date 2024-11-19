8901339e150cd7ab3f38b8649ef34bbc02e7e7fb
Jafar Loka <gaafer.hajji1995@gmail.com>
2024-11-19 21:15:38 +0200

    Register Model Inside Admin Panel Site And Change Model Display Name

--- my_api/first_api/admin.py
+++ my_api/first_api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import JLCarSpecs;
+
+admin.site.register(JLCarSpecs);
+
 # Register your models here.
--- my_api/first_api/models.py
+++ my_api/first_api/models.py
@@ -7,3 +7,6 @@ class JLCarSpecs(models.Model):
     production_year     = models.CharField(max_length=10);
     car_body            = models.CharField(max_length=100);
     engine_type         = models.CharField(max_length=50);
+
+    def __str__(self):
+        return self.car_brand + " - " + self.engine_type;
